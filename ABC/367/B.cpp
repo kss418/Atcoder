fe95103be3cc6925441307f58f9c086d8d32b5e6
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-17 22:14:18 +0900

    Create B.cpp

---
+++ ABC/367/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using pll = pair<ll, ll>; ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101;
+ll arr[MAX];
+
+
+int main()
+{
+    fastio;
+    long double num;
+    cin >> num;
+    cout << num;
+    
+
+    return 0;
+}
