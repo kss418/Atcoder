0a8b2abe433f430388618c6068523296f52db222
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-18 03:43:31 +0900

    Create A.cpp

---
+++ ABC/314/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
+using namespace std; typedef long long ll; 
+typedef pair<int, int> pi; typedef pair<ll, ll> pll; 
+typedef unsigned long long ull; ll n, m, k, t; string s;
+        
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ll MAX = 201010;
+ll arr[MAX], sum;
+string pii = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+
+int main() { 
+    fastio;
+    
+    cin >> n;
+    for(int i = 0;i <= n + 1;i++) cout << pii[i];
+    
+    
+
+    return 0;
+}
+
