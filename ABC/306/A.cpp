55eb7d36f1be47a91759b3e073bcfdec4aef9c9b
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-18 03:42:41 +0900

    Create A.cpp

---
+++ ABC/306/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define fastio 	cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+typedef pair<int, int> pi; typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+ll n, m, k, t; string s;
+
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MAX = 501;
+
+
+int main() {
+	fastio;
+    
+    cin >> n >> s;
+    for(auto i : s) cout << i << i;
+    
+    
+    
+	return 0;
+}
