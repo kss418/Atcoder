cb0769bb1896f5e40d9fe1d00370b5edadabbcd7
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-18 02:44:07 +0900

    Create A.cpp

---
+++ ABC/281/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	for (int i = n; i >= 0; i--) {
+		cout << i << "\n";
+	}
+
+
+	return 0;
+}
+
