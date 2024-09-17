3b340a824de58e7f26303028dd9b50d146fde7fb
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-18 02:04:54 +0900

    Create A.cpp

---
+++ ABC/266/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	string s;
+	cin >> s;
+	cout << s[(s.size() - 1) / 2];
+
+
+
+	
+	return 0;
+}
+
