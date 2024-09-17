724adc7ea7708db82669f57c009890792c7d4c25
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-17 23:52:22 +0900

    Create A.cpp

---
+++ ABC/238/A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <map>
+#include <stack>
+using namespace std; typedef long long ll;
+typedef pair<int, int> pi; typedef pair<ll, ll> pll;
+ll n, m, k, t; string s;
+
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MAX = 1010;
+
+
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	cin >> n;
+	if (n != 2 && n != 3 && n != 4) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
+
+
+
+
+	return 0;
+}
