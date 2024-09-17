b2779c0988adcc409cde069ffbbb51dc640801e3
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-17 09:27:20 +0900

    Create B.cpp

---
+++ ABC/363/B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101;
+constexpr ll MOD = 1e9 + 7;
+ll arr[MAX];
+
+int main() {
+    fastio;
+   
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+    sort(arr + 1, arr + n + 1); reverse(arr + 1, arr + n + 1);
+
+    cout << max<ll>(0, m - arr[k]);
+
+    return 0;
+}
