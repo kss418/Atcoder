d0130c554bd71c132fafb82b25ba65419b83f68b
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-18 02:42:06 +0900

    Update B.cpp

--- ABC/280/B.cpp
+++ ABC/280/B.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cstring>
-#include <set>
-#include <queue>
-#include <deque>
 using namespace std;
 typedef long long ll;
-ll n, m, k, t; string s;
 
-const int MAX = 1010;
 
 
 int main() {
@@ -17,32 +11,19 @@ int main() {
 	cout.tie(0);
 	ios::sync_with_stdio(false);
 
-	string r;
-	cin >> s >> r;
-	int flag = 0;
-	for (int i = 0; i < s.size(); i++) {
-		for (int j = 0; j < r.size(); j++) {
-			if (s[i + j] == r[j]) {
-				if (j == r.size() - 1) {
-					flag = 1;
-					break;
-				}
-			}
-			else {
-				break;
-			}
-		}
+	int n;
+	cin >> n;
+	int arr[11];
+	arr[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
 	}
 
-	if (flag) {
-		cout << "Yes";
+	for (int i = 1; i <= n; i++) {
+		cout << arr[i] - arr[i - 1] << " ";
 	}
-	else {
-		cout << "No";
-	}
-
 	
-
 
 	return 0;
 }
+
