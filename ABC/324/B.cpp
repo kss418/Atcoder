3302242da834c75b69b4e2cfbae46f32babe1c11
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-04 22:52:18 +0900

    Create B.cpp

---
+++ ABC/324/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+
+int main() {
+    fastio;
+
+    cin >> n;
+    while(n % 2 == 0) n /= 2;
+    while(n % 3 == 0) n /= 3;
+
+    if(n > 1) cout << "No";
+    else cout << "Yes";
+    
+
+    return 0;
+}
