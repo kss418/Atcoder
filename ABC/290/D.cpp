bd15146839e62042ce0511eb4d2e8b40e82c2fa5
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-17 22:18:28 +0900

    Create D.cpp

---
+++ ABC/290/D.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101010;
+constexpr ll MOD = 998244353;
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tuple <ll, ll, ll> init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+};
+
+int main() {
+    fastio;
+
+    cin >> t; _gcd gcd;
+    while(t--){
+        ll d; cin >> n >> d >> k; 
+        d %= n; k--;
+        ll g = gcd.ret(n, d);
+        ll turn = n / g;
+        ll pl = k / turn;
+        ll seq = k % turn;
+
+        ll cur = (pl + (seq * d)) % n;
+        cout << cur << "\n";
+    }
+
+    return 0;
+}
