10d65db2edfbab57176408f5d526e9c351c3619a
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-29 19:44:04 +0900

    Create D.cpp

---
+++ ABC/358/D.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+ll a[MAX], b[MAX];
+
+int main() {
+    fastio;
+
+
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= m;i++) cin >> b[i];
+    sort(a + 1, a + n + 1);
+    sort(b + 1, b + m + 1);
+
+    ll aidx = 1, bidx = 1, result = 0;
+    while(aidx <= n && bidx <= m){
+        if(a[aidx] >= b[bidx]) result += a[aidx], bidx++;
+        aidx++;
+    }
+
+    if(bidx == m + 1) cout << result;
+    else cout << -1;
+    
+
+    return 0;
+}
+
