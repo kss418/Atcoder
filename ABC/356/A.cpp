4c3c93530f0c94f0b1983d8bffc68375acf73342
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-30 18:41:04 +0900

    Create A.cpp

---
+++ ABC/356/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+vector <ll> a;
+
+int main() {
+    fastio;
+
+
+    cin >> n >> m >> k;
+    a.resize(n); iota(all(a), 1);
+    reverse(a.begin() + m - 1, a.begin() + k);
+
+    for(auto& i : a) cout << i << " ";
+
+    return 0;
+}
+
