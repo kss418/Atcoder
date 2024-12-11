f5fa9cfbf5924a5d5de1cc75a0eac7ae5ae592f8
kss418 <108602943+kss418@users.noreply.github.com>
2024-12-11 21:22:32 +0900

    Create F.cpp

---
+++ ABC/351/F.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 401010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+class _sseg {
+public:
+    ll n; vector <ll> arr, seg;
+    _sseg(ll n) {
+        this->n = n;
+        arr.resize(n + 1); seg.resize(4 * n + 1);
+    }
+
+    void con(ll idx, ll val) { arr[idx] = val; }
+
+    void init() { init(1, n); }
+    ll init(ll l, ll r, ll node = 1) {
+        if (l == r) return seg[node] = arr[l];
+        ll mid = (l + r) >> 1;
+        seg[node] = init(l, mid, node * 2) + init(mid + 1, r, node * 2 + 1);
+        return seg[node];
+    }
+
+    ll query(ll st, ll en) { return query(st, en, 1, n); }
+    ll query(ll st, ll en, ll l, ll r, ll node = 1) {
+        if (en < l || st > r) return 0;
+        if (st <= l && en >= r) return seg[node];
+        ll mid = (l + r) >> 1;
+        return query(st, en, l, mid, node * 2) + query(st, en, mid + 1, r, node * 2 + 1);
+    }
+
+    void add(ll idx, ll val) { add(idx, val, 1, n); }
+    void add(ll idx, ll val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return;
+        seg[node] += val;
+        if (l == r) return;
+        ll mid = (l + r) >> 1;
+        add(idx, val, l, mid, node * 2);
+        add(idx, val, mid + 1, r, node * 2 + 1);
+    }
+
+    ll update(ll idx, ll val) { return update(idx, val, 1, n); }
+    ll update(ll idx, ll val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return seg[node];
+        if (l == r) return seg[node] = val;
+        ll mid = (l + r) >> 1;
+
+        seg[node] = update(idx, val, l, mid, node * 2);
+        seg[node] += update(idx, val, mid + 1, r, node * 2 + 1);
+        return seg[node];
+    }
+
+    ll cnt(ll val) { return cnt(val, 1, n); }
+    ll cnt(ll val, ll l, ll r, ll node = 1){
+        if(l == r) return r;
+        ll mid = (l + r) >> 1;
+
+        if(seg[node * 2] < val) return cnt(val - seg[node * 2], mid + 1, r, node * 2 + 1);
+        else return cnt(val, l, mid, node * 2);
+    }
+};
+
+ll org[MAX], c;
+vector <ll> arr;
+map <ll, ll> num;
+
+int main() {
+    fastio;
+
+    cin >> n; _sseg seg(MAX), cnt(MAX);
+    for(int i = 1;i <= n;i++) cin >> a[i], arr.push_back(a[i]);
+    sort(all(arr));
+
+    for(auto& i : arr){
+        if(num.count(i)) continue;
+        num[i] = ++c; org[c] = i;
+    }
+
+    for(int i = 1;i <= n;i++) a[i] = num[a[i]];
+
+    ll result = 0;
+    for(int i = n;i >= 1;i--){
+        ll cur = org[a[i]];
+        result += seg.query(a[i] + 1, MAX);
+        result -= cur * cnt.query(a[i] + 1, MAX);
+        seg.add(a[i], cur); cnt.add(a[i], 1);
+    }
+
+    cout << result;
+
+     
+    return 0;
+}
