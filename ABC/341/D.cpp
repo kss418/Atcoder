66835a369a068971068725a087359eb0c95c3931
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-03 17:27:15 +0900

    Create D.cpp

---
+++ ABC/341/D.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tll init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+} g;
+
+template <typename T = ll>
+class _bs { // 0-based index
+public:
+    T l, r;
+    _bs(T l, T r) {
+        this->l = l; this->r = r;
+    }
+
+    bool f(T cur) {
+        ll cnt = cur / n + cur / m - 2 * (cur / ((n * m) / g.ret(n, m)));
+        return cnt < k;
+    }
+
+    T ret() {
+        T lo = l, hi = r, mid = -1;
+        while (hi > lo) {
+            mid = (hi + lo) / (T)2;
+            if (f(mid)) lo = mid + 1;
+            else hi = mid;
+        }
+
+        return lo;
+    }
+};
+
+void run(){
+    cin >> n >> m >> k;
+    _bs <ll> bs(1ll, 2e18);
+
+    cout << bs.ret();
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
