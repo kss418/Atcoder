d1243af0e427511e45660280b02633bdaae6097b
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-12 21:39:11 +0900

    Create F.cpp

---
+++ ABC/334/F.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+pll a[MAX], st; ld d[MAX];
+
+template <typename T = ld> // query type
+class _seg { // 구간 예외 처리하기
+public:
+    ll n;
+    class node{
+    public:
+        ld v;
+        node() : node(1e18){}
+        node(ld v) {
+            this->v = v;
+        }
+
+        node operator + (node ot){ // update
+            ld now = min(v, ot.v);
+            return node(now);
+        }
+
+        node operator += (T ot){ // add
+            v += ot;
+            return node(v);
+        }
+
+        operator T(){ // update -> query
+            return v;
+        }
+    };
+    vector <node> seg;
+
+    _seg(){}
+    _seg(ll n) {
+        this->n = n;
+        seg.resize(4 * n + 1, node());
+    }
+
+    T query(ll st, ll en) { return query(st, en, 0, n); }
+    node query(ll st, ll en, ll l, ll r, ll node = 1) {
+        if (en < l || st > r) return _seg::node();
+        if (st <= l && en >= r) return seg[node];
+        ll mid = (l + r) >> 1;
+        return query(st, en, l, mid, node * 2) + query(st, en, mid + 1, r, node * 2 + 1);
+    }
+
+    node update(ll idx, ld val) { return update(idx, val, 0, n); }
+    node update(ll idx, ld val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return seg[node];
+        if (l == r) return seg[node] = val;
+        ll mid = (l + r) >> 1;
+
+        seg[node] = update(idx, val, l, mid, node * 2) + update(idx, val, mid + 1, r, node * 2 + 1);
+        return seg[node];
+    }
+
+    void add(ll idx, ll val) { add(idx, val, 0, n); }
+    void add(ll idx, ll val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return;
+        seg[node] += val;
+        if (l == r) return;
+        ll mid = (l + r) >> 1;
+
+        add(idx, val, l, mid, node * 2);
+        add(idx, val, mid + 1, r, node * 2 + 1);
+    }
+};
+
+ld dist(pll v1, pll v2){
+    ll dx = v1.x - v2.x;
+    ll dy = v1.y - v2.y;
+    return sqrtl(dx * dx + dy * dy);
+}
+
+void run(){
+    cin >> n >> m >> a[0].x >> a[0].y;
+    a[n + 1] = a[0]; _seg seg(n + 1);
+    for(int i = 1;i <= n;i++) cin >> a[i].x >> a[i].y;
+
+    ld result = dist(a[0], a[1]);
+    for(int i = 1;i <= n;i++) {
+        ld nxt = dist(a[i], a[i + 1]);
+        ld home = dist(a[i], a[0]) + dist(a[i + 1], a[0]);
+        d[i] = home - nxt; result += nxt;
+    }
+
+    seg.update(0, 0);
+    for(int i = 1;i <= n + 1;i++){
+        seg.update(i, seg.query(max(0ll, i - m), i) + d[i - 1]);
+    }
+
+    result += seg.query(n + 1, n + 1);
+    cout.precision(20); cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
