5c60686a878a00e9e712d28e9cc958037ab86e0e
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-20 14:26:35 +0900

    Create B.cpp

---
+++ ABC/328/B.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], result;
+
+bool chk(ll a, ll b){
+    string s = to_string(a) + to_string(b);
+    for(int i = 1;i < s.size();i++){
+        if(s[i] != s[i - 1]) return 0;
+    }
+
+    return 1;
+}
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= a[i];j++){
+            result += chk(i, j);
+        }
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
