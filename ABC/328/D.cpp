d35bace8a4cb9d33bcb37e7465284547531a0b71
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-20 14:26:49 +0900

    Create D.cpp

---
+++ ABC/328/D.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+vector <char> arr;
+
+void pop(){
+    if(arr.size() < 3) return;
+    if(arr.back() != 'C') return;
+    if(arr[arr.size() - 2] != 'B') return;
+    if(arr[arr.size() - 3] != 'A') return;
+    for(int i = 1;i <= 3;i++) arr.pop_back();
+}
+
+void run(){
+    cin >> s;
+    for(auto& i : s){
+        arr.push_back(i); pop();
+    }
+
+    for(auto& i : arr) cout << i;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
